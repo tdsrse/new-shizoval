86cb8f2265218bba33484142f0ed102b740397cb
tdsrse <162815107+tdsrse@users.noreply.github.com>
2024-03-09 23:31:09 +0700

    Create shizoval-new.js

---
+++ shizoval-new.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name             shizoval new
+// @author           Alastor, tdsrse, sheezzmee
+// @version          2.0.3
+
+// @match            https://*.test-eu.tankionline.com/browser-public/index.html?*
+// @match            https://tankionline.com/play*
+
+// @description      Shizoval: Reborn
+// @icon             https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
+
+// @grant            unsafeWindow
+// @grant            GM_addStyle
+// @run-at           document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // soon
+})();
